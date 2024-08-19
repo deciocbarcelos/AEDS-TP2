8a53bec83b88721ff4331aa29475405c6ee586a6
deciocbarcelos <decio.barcelos@ufv.br>
2024-08-19 14:23:13 -0300

    Implementações dos diferentes tipos de quick sort

--- ordenacao.c
+++ ordenacao.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // Variável de controle para o modo verbose
 int verbose = 0; // 1 para ativar, 0 para desativar
@@ -151,4 +152,136 @@ void quickSortIterativo(Item* A, int n) {
 
     // Libera a memória da pilha
     free(stack);
+}
+int medianaDeTres(Item* v, int a, int b, int c) {
+    if ((v[a].chave > v[b].chave) != (v[a].chave > v[c].chave))
+        return a;
+    else if ((v[b].chave > v[a].chave) != (v[b].chave > v[c].chave))
+        return b;
+    else
+        return c;
+}
+
+void particaoMediana3(int Esq, int Dir, int* i, int* j, Item* A) {
+    // Seleciona três índices aleatórios
+    int k1 = Esq + rand() % (Dir - Esq + 1);
+    int k2 = Esq + rand() % (Dir - Esq + 1);
+    int k3 = Esq + rand() % (Dir - Esq + 1);
+
+    // Determina o índice do pivô usando a mediana de três
+    int pivo_indice = medianaDeTres(A, k1, k2, k3);
+    Item pivo = A[pivo_indice];
+    
+    // Particionamento
+    *i = Esq;
+    *j = Dir;
+    
+    do {
+        while (A[*i].chave < pivo.chave) (*i)++;
+        while (A[*j].chave > pivo.chave) (*j)--;
+        if (*i <= *j) {
+            Item aux = A[*i];
+            A[*i] = A[*j];
+            A[*j] = aux;
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+}
+
+void ordenaMediana3(int Esq, int Dir, Item* A) {
+    int i, j;
+    particaoMediana3(Esq, Dir, &i, &j, A);
+    if (Esq < j) ordenaMediana3(Esq, j, A);
+    if (i < Dir) ordenaMediana3(i, Dir, A);
+}
+
+void quickSortMediana3(Item* A, int n) {
+    srand(time(NULL));  // Inicializa a semente do gerador de números aleatórios
+    ordenaMediana3(0, n - 1, A);
+}
+void quickSortComInsercao(Item* A, int n) {
+    ordena(0, n - 1, A);
+}
+
+// Função para encontrar a mediana de 5 elementos
+int medianaDeCinco(Item* v, int a, int b, int c, int d, int e) {
+    Item arr[] = {v[a], v[b], v[c], v[d], v[e]};
+    
+    // Ordena os cinco elementos para encontrar a mediana
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (arr[j].chave < arr[i].chave) {
+                Item temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    // A mediana é o elemento do meio
+    return arr[2].chave;
+}
+void particaoMediana5(int Esq, int Dir, int* i, int* j, Item* A) {
+    // Seleciona cinco índices aleatórios
+    int k1 = Esq + rand() % (Dir - Esq + 1);
+    int k2 = Esq + rand() % (Dir - Esq + 1);
+    int k3 = Esq + rand() % (Dir - Esq + 1);
+    int k4 = Esq + rand() % (Dir - Esq + 1);
+    int k5 = Esq + rand() % (Dir - Esq + 1);
+
+    // Determina a mediana de cinco
+    int pivo = medianaDeCinco(A, k1, k2, k3, k4, k5);
+    
+    // Particionamento
+    *i = Esq;
+    *j = Dir;
+
+    do {
+        while (A[*i].chave < pivo) (*i)++;
+        while (A[*j].chave > pivo) (*j)--;
+        if (*i <= *j) {
+            Item aux = A[*i];
+            A[*i] = A[*j];
+            A[*j] = aux;
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+
+    if (verbose) {
+        printf("ParticaoMediana5: Esq=%d, Dir=%d, i=%d, j=%d, Pivô=%d\n", Esq, Dir, *i, *j, pivo);
+        print_vetor(A, Dir - Esq + 1);
+    }
+}
+
+void ordenaMediana5(int Esq, int Dir, Item* A) {
+    int i, j;
+    particaoMediana5(Esq, Dir, &i, &j, A);
+    if (Esq < j) ordenaMediana5(Esq, j, A);
+    if (i < Dir) ordenaMediana5(i, Dir, A);
+}
+
+void quickSortMediana5(Item* A, int n) {
+    srand(time(NULL));  // Inicializa a semente do gerador de números aleatórios
+    ordenaMediana5(0, n - 1, A);
+}
+void executar_experimento(Item* v, int n, void (*sort_func)(Item*, int)) {
+    long long int comparacoes = 0;
+    long long int copias = 0;
+
+    clock_t inicio = clock();
+    sort_func(v, n);
+    clock_t fim = clock();
+
+    double tempo_execucao = (double)(fim - inicio) / CLOCKS_PER_SEC;
+
+    printf("Tempo de Execução: %.3f segundos\n", tempo_execucao);
+    printf("Comparações: %lld\n", comparacoes);
+    printf("Cópias: %lld\n", copias);
+}
+void gerar_vetor_aleatorio(Item* v, int n) {
+    for (int i = 0; i < n; i++) {
+        v[i].chave = rand() % 1000000;
+    }
 }
--- ordenacao.h
+++ ordenacao.h
@@ -21,4 +21,13 @@ void Ordena(int Esq, int Dir, Item *A);
 void QuickSort_iterativo(Item *A, int n);
 void Particao(int Esq, int Dir, int *i, int *j, Item *A);
 void print_vetor(Item* v, int n);
+void quickSortComInsercao(Item* A, int n);
+int medianaDeCinco(Item* v, int a, int b, int c, int d, int e);
+void quickSortMediana5(Item* A, int n);
+void ordenaMediana5(int Esq, int Dir, Item* A);
+void particaoMediana5(int Esq, int Dir, int* i, int* j, Item* A);
+void executar_experimento(Item* v, int n, void (*sort_func)(Item*, int));
+void gerar_vetor_aleatorio(Item* v, int n);
+
+
 #endif
